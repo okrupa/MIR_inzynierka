5fa3487dd9292f7b9c2cb2a763eafff3b4eae03c
Karol <karol@Karols-MacBook-Pro-2.local>
2022-12-04 23:10:26 +0100

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MIR BEng Thesis "Etykietowanie muzyki (music tagging) za pomocą metod sztucznej inteligencji"',
+    author='Olga Krupa',
+    license='',
+)
